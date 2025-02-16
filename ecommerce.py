ce34e155d5beec5c1462ad6f844a478098c42e2a
RaghavChawla-chitkara <raghav2850.beai24@chitkara.edu.in>
2025-02-16 13:19:37 +0530

    check2

--- ecommerce.py
+++ ecommerce.py
@@ -11,8 +11,6 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'  # Dynamically Generated Secret Ke
 app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
-# hello
-
 # -------------------- Models --------------------
 # User Model for Authentication
 class User(db.Model):
